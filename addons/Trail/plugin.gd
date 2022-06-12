02962c7495a6d7d0c0531360fc344f527f5530f9
Steam Deck User <deck@steamdeck>
2022-06-12 11:20:45 +0200

    Install Trail Addon

---
+++ addons/Trail/plugin.gd
@@ -0,0 +1,8 @@
+tool
+extends EditorPlugin
+
+func _enter_tree():
+	add_custom_type("Trail2D","Line2D",preload("res://addons/Trail/trail_2d.gd"),preload("res://addons/Trail/trail2d_icon.svg"))
+
+func _exit_tree():
+	remove_custom_type("Trail2D")
