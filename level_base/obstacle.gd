b5a64903f026b6f91d4569ebadf6e3e3c4eaba3e
Skunkynator <43096109+Skunkynator@users.noreply.github.com>
2022-06-26 20:14:11 +0200

    updated the obstacle to use sprites instead of mesh 2D

--- level_base/obstacle.gd
+++ level_base/obstacle.gd
@@ -4,28 +4,24 @@ extends Area2D
 
 export var obj_size := Vector2(10,10) setget set_obj_size
 var shape : RectangleShape2D
-var mesh_inst : MeshInstance2D
-var mesh : QuadMesh
+var visual : Node2D
 
 
 func _ready() -> void:
-	mesh = QuadMesh.new()
 	shape = RectangleShape2D.new()
-	mesh.size = obj_size
 	shape.extents = obj_size / 2
 	var col_shape := get_node_or_null("Collision") as CollisionShape2D
 	if col_shape:
 		col_shape.shape = shape
-	var mesh_inst = get_node_or_null("Visual") as MeshInstance2D
-	if mesh_inst:
-		mesh_inst.mesh = mesh
+	visual = get_node_or_null("Visual") as Node2D
+	visual.scale = obj_size
 	if not Engine.editor_hint:
 		set_script(null)
 
 
 func set_obj_size(size : Vector2) -> void:
-	if mesh:
-		mesh.size = size
+	if visual:
+		visual.scale = size
 	if shape:
 		shape.extents = size / 2
 	obj_size = size
